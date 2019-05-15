981269a8d9cd15901a97fc5aae1d2e68f7be2a75
Thomas Bouvier <thomas.bouvier@snips.ai>
2019-05-15 17:14:58 +0200

    Updated dependencies + disabled debug printing

--- action-directions.js
+++ action-directions.js
@@ -2,8 +2,9 @@
 const debug = require('debug')
 const { name } = require('./package.json')
 
+// Enable error print
+debug.enable(name + ':error')
 // Uncomment this line to print everything
-//debug.enable(name + ':*')
 debug.enable(name + ':error')
 
 require('./src/index')()
