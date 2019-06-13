88937ec7967010f51b8cb5084f318f984b2b200b
Thomas Bouvier <thomas.bouvier@snips.ai>
2019-06-13 14:14:57 +0200

    Updated dependencies

--- action-directions.js
+++ action-directions.js
@@ -3,8 +3,7 @@ const debug = require('debug')
 const { name } = require('./package.json')
 
 // Uncomment this line to print everything
-debug.enable(name + ':*')
-
-//debug.enable(name + ':info')
+//debug.enable(name + ':*')
+debug.enable(name + ':error')
 
 require('./src/index')()
